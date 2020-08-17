3cc4f53a015c2e076924f997975b8acf09e01eb5
tdimitri <62962217+tdimitri@users.noreply.github.com>
2020-08-17 09:32:12 -0500

    remove scipy depenndency

--- riptable/benchmarks/analysis.py
+++ riptable/benchmarks/analysis.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numbers
 import numpy as np
-import scipy.stats as st
 
 from typing import Any, Callable, Mapping, Tuple, Union, Generator
 from .results import RunResult
@@ -193,31 +192,32 @@ def _analyze_results(run_nano_times: np.ndarray) -> RunResult:
     est_mean, est_variance = jackknife_mean_and_var(run_nano_times)
 
     # TEMP: While developing the estimator code for benchmarking, make it easy to enable/disable
-    if enable_bench_estimators:
-        # Calculate the cumulative sums of the timing samples, then perform
-        # a linear regression to fit a line to the cumsums. The more stable
-        # the timing results, the higher (closer to 1) the R^2 value will be.
-        timing_cumsums = run_nano_times.view(FastArray).cumsum()
-        iteration_numbers = np.arange(len(run_nano_times))
-        slope, intercept, r_value, p_value, std_err = st.linregress(
-            iteration_numbers, timing_cumsums
-        )
 
-        # TEMP: Print out regression results for debugging.
-        print(
-            f"slope: {slope}\tintercept: {intercept}\tr_value: {r_value}\tp_value: {p_value}\tstd_err: {std_err}"
-        )
-
-        # TEMP: Use bootstrap to produce 95% confidence interval (alpha=0.05) estimates of min/median/max.
-        agg_funcs = {"min": np.min, "median": np.median, "max": np.max, "stdev": np.std}
-        rng = np.random.default_rng()
-        bootstrap_aggs = _bootstrap_resample(
-            rng, run_nano_times, 10 * len(run_nano_times), agg_funcs
-        )
-
-        # Print the median and confidence interval endpoints for each of the aggregates.
-        for key, agg_arr in bootstrap_aggs.items():
-            pcts = np.nanpercentile(agg_arr, [2.5, 50.0, 97.5])
-            print(f"Bootstrap (95% CI)[{key}]: {pcts[1]} ({pcts[0]} ... {pcts[2]})")
+    #if enable_bench_estimators:
+    #    # Calculate the cumulative sums of the timing samples, then perform
+    #    # a linear regression to fit a line to the cumsums. The more stable
+    #    # the timing results, the higher (closer to 1) the R^2 value will be.
+    #    timing_cumsums = run_nano_times.view(FastArray).cumsum()
+    #    iteration_numbers = np.arange(len(run_nano_times))
+    #    slope, intercept, r_value, p_value, std_err = st.linregress(
+    #        iteration_numbers, timing_cumsums
+    #    )
+    #
+    #    # TEMP: Print out regression results for debugging.
+    #    print(
+    #        f"slope: {slope}\tintercept: {intercept}\tr_value: {r_value}\tp_value: {p_value}\tstd_err: {std_err}"
+    #    )
+    #
+    #    # TEMP: Use bootstrap to produce 95% confidence interval (alpha=0.05) estimates of min/median/max.
+    #    agg_funcs = {"min": np.min, "median": np.median, "max": np.max, "stdev": np.std}
+    #    rng = np.random.default_rng()
+    #    bootstrap_aggs = _bootstrap_resample(
+    #        rng, run_nano_times, 10 * len(run_nano_times), agg_funcs
+    #    )
+    #
+    #    # Print the median and confidence interval endpoints for each of the aggregates.
+    #    for key, agg_arr in bootstrap_aggs.items():
+    #        pcts = np.nanpercentile(agg_arr, [2.5, 50.0, 97.5])
+    #        print(f"Bootstrap (95% CI)[{key}]: {pcts[1]} ({pcts[0]} ... {pcts[2]})")
 
     return RunResult(min_time, med_time, max_time, int(est_mean), est_variance)
