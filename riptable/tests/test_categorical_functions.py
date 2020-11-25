b1bf1c70b0c7d6e1ca061740b47d3a3651aab499
Jack Pappas <jack-pappas@users.noreply.github.com>
2020-11-25 14:53:34 -0500

    Parameterize TestCategoricalSimpleGroupBy.test_single_col_categoricals.
    Parameterize TestCategoricalSimpleGroupBy.test_single_col_categoricals so
    each (function, dtype) pair is run as it's own test case; this makes it
    easier to see which pairs are passing and which aren't (instead of halting
    on the first failure). Utilize an assertion function from numpy.testing
    to provide a richer error message for arrays which aren't equal.

--- riptable/tests/test_categorical_functions.py
+++ riptable/tests/test_categorical_functions.py
@@ -1,5 +1,9 @@
+import numpy as np
 import pandas as pd
 import riptable as rt
+
+import pytest
+from numpy.testing import assert_array_almost_equal
 
 
 def count(df, data=None):
@@ -85,7 +89,6 @@ functions_str = [
     # 'cummax',
     # 'cummin'
 ]
-import numpy as np
 
 type_list = [
     # np.bool,          ## not a numeric type
@@ -106,54 +109,32 @@ type_list = [
     # np.complex128     ## not supported
 ]
 
-
-def safe_equal(ary1, ary2):
-    def isNaN(num):
-        return num != num
-
-    assert len(ary1) == len(ary2)
-
-    for a, b in zip(ary1, ary2):
-        epsilon = 0.001  ##high epsilon for comparing float32 and float64
-        if not ((abs(a - b) < epsilon) or (isNaN(a) and isNaN(b))):
-            return False
-    return True
-
-
 class TestCategoricalSimpleGroupBy:
-    def groupby_func(self, df, fn, data=None):
-        return functions[fn](df, data)
+    def groupby_func(self, df, fn, data):
+        return fn(df, data)
 
-    # TODO pytest parameterize across type_list and functions
-    def test_single_col_categoricals(self):
+    @pytest.mark.parametrize(('func', 'func_name'), [
+        pytest.param(x, y, id=y) for (x, y) in zip(functions, functions_str)
+    ])
+    @pytest.mark.parametrize('data_dtype', type_list)
+    def test_single_col_categoricals(self, func, func_name: str, data_dtype):
         values = [0, 1, 1, 2, 2, 2, 3, 3, 3, 4]
         bin_ids = ['a', 'b', 'c', 'd', 'e']
-        data = np.random.rand(10) + np.random.randint(0, 10, size=10)
+        #data = np.random.rand(10) + np.random.randint(0, 10, size=10)
+        data = np.array([
+            7.19200901, 0.14907245, 2.28258397, 5.07872708, 0.76125165,
+            1.32797916, 3.40280423, 4.48942476, 6.98713656, 4.39541456])
 
-        for type_ in type_list:
-            data = rt.FastArray(data, dtype=type_)
+        data = rt.FastArray(data, dtype=data_dtype)
 
-            map = {'vs': data, 'ks': values}
+        map = {'vs': data, 'ks': values}
 
-            pd_data = pd.DataFrame(map).groupby(by='ks')
-            sfw_data = rt.Categorical(values=values, categories=bin_ids, base_index=0)
+        pd_data = pd.DataFrame(map).groupby(by='ks')
+        rt_data = rt.Categorical(values=values, categories=bin_ids, base_index=0)
 
-            for function in range(0, len(functions)):
-                pd_out = self.groupby_func(pd_data, function)
-                sfw_out = self.groupby_func(sfw_data, function, data)
+        pd_out = self.groupby_func(pd_data, func, None)
+        rt_out = self.groupby_func(rt_data, func, data)
 
-                col_index = 'Count' if functions_str[function] == 'count' else 0
+        col_index = 'Count' if func_name == 'count' else 0
 
-                if (
-                    not safe_equal(list(sfw_out[col_index]), list(pd_out['vs']))
-                    and function != 'median'
-                    and not np.issubdtype(type_, np.integer)
-                ):
-                    print(sfw_out)
-                    print(pd_out)
-
-                    # TODO - rework this so assert messages are used
-                    print(list(sfw_out[col_index]))
-                    print(list(pd_out['vs']))
-                    print('Function failed on - ', functions_str[function])
-                    assert False
+        assert_array_almost_equal(rt_out[col_index], pd_out['vs'].values, decimal=3)
