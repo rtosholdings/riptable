7b78030bc7da86116c82559c6d07ea4825eea07a
Condron <Eoin.Condron@sig.com>
2021-01-04 12:29:22 +0000

    Add tests for FAString.contains. Also make SYMBOLS as global variable

--- riptable/tests/test_str.py
+++ riptable/tests/test_str.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 
 from riptable import *
 
@@ -11,11 +12,13 @@ def arr_all(a):
     return bool(np.all(a))
 
 
+SYMBOLS = ['AAPL', 'AMZN', 'FB', 'GOOG', 'IBM']
+
+
 class StrTest(unittest.TestCase):
     def test_cat(self):
         arrsize = 200
-        symbols = ['AAPL', 'AMZN', 'FB', 'GOOG', 'IBM']
-        symbol = Cat(1 + arange(arrsize) % len(symbols), symbols)
+        symbol = Cat(1 + arange(arrsize) % len(SYMBOLS), SYMBOLS)
         result = symbol.expand_array.str.startswith('AAPL') == symbol.str.startswith(
             'AAPL'
         )
@@ -26,6 +29,27 @@ class StrTest(unittest.TestCase):
         self.assertTrue(np.all(result == [False, False, True]))
         result = FAString(['abab', 'ababa', 'abababb']).endswith('ba')
         self.assertTrue(np.all(result == [False, True, False]))
+
+
+regexpb_test_cases = pytest.mark.parametrize('str2, expected', [
+    ('.', [True] * 5),
+    ('\.', [False] * 5),
+    ('A', [True, True, False, False, False]),
+    ('[A|B]', [True, True, True, False, True]),
+    ('B$', [False, False, True, False, False]),
+])
+
+
+@regexpb_test_cases
+def test_regexpb(str2, expected):
+    fa = FA(SYMBOLS)
+    assert np.array_equal(fa.str.regexpb(str2), expected)
+
+
+@regexpb_test_cases
+def test_regexpb_cat(str2, expected):
+    cat = Cat(SYMBOLS * 2)   # introduce duplicity to test ikey properly
+    assert np.array_equal(cat.str.regexpb(str2), expected * 2)
 
 
 if __name__ == '__main__':
