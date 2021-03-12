a436b46e24ee1ed0404c5581e5e4f4327cab7908
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2021-03-12 11:11:52 -0500

    Update rt_timezone.py
    np.bool to np.bool_
    removes around 300 warnings

--- riptable/rt_timezone.py
+++ riptable/rt_timezone.py
@@ -429,7 +429,7 @@ class TimeZone():
         #is_dst = (FastArray(np.searchsorted(DST_CUTOFFS, arr)) & 1).astype(np.bool)
         #is_dst = (rc.BinsToCutsBSearch(arr, cutoffs, 0) & 1).astype(np.bool)
 
-        is_dst = (searchsorted(cutoffs, arr) & 1).astype(np.bool)
+        is_dst = (searchsorted(cutoffs, arr) & 1).astype(np.bool_)
         return is_dst
 
     #------------------------------------------------------------
