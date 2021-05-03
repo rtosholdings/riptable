3d1611416eec04a155ae685c615d63f9a3e645a4
Jack Pappas <jack-pappas@users.noreply.github.com>
2021-05-03 17:11:21 -0400

    Consolidate numba string functions so only one definition is needed per function.
    Some string-related numba functions were duplicated to provide both a serial and parallel
    implementation. This change consolidates those implementations so each function only needs
    one definition, which is then specialized to provide both a serial and parallel version.

--- riptable/tests/test_str.py
+++ riptable/tests/test_str.py
@@ -1,8 +1,12 @@
-
 import pytest
 parametrize = pytest.mark.parametrize
 
 from riptable import *
+
+
+from numpy.testing import assert_array_equal
+from ..testing.array_assert import assert_array_or_cat_equal
+
 
 
 SYMBOLS = ['AAPL', 'AMZN', 'FB', 'GOOG', 'IBM']
@@ -17,16 +21,14 @@ class TestStr:
     def test_cat(self):
         arrsize = 200
         symbol = Cat(1 + arange(arrsize) % len(SYMBOLS), SYMBOLS)
-        result = symbol.expand_array.str.startswith('AAPL') == symbol.str.startswith(
+        assert_array_equal(symbol.expand_array.str.startswith('AAPL'), symbol.str.startswith(
             'AAPL'
-        )
-        assert np.all(result)
+        ))
 
     def test_cat_filtered(self):
-        result = self.cat_symbol.expand_array.str.startswith('IBM') == self.cat_symbol.str.startswith(
+        assert_array_equal(self.cat_symbol.expand_array.str.startswith('IBM'), self.cat_symbol.str.startswith(
             'IBM'
-        )
-        assert np.all(result)
+        ))
 
     def test_lower(self):
         result = FAString(SYMBOLS).lower
@@ -35,7 +37,7 @@ class TestStr:
     def test_lower_cat(self):
         result = self.cat_symbol.str.lower
         expected = Cat(self.cat_symbol.ikey, [s.lower() for s in SYMBOLS])
-        assert (result == expected).all()
+        assert_array_or_cat_equal(result, expected, relaxed_cat_check=True)
 
     def test_upper(self):
         result = FAString(SYMBOLS).upper
@@ -44,7 +46,7 @@ class TestStr:
     def test_upper_cat(self):
         result = self.cat_symbol.str.upper
         expected = Cat(self.cat_symbol.ikey, [s.upper() for s in SYMBOLS])
-        assert (result == expected).all()
+        assert_array_or_cat_equal(result, expected, relaxed_cat_check=True)
 
     @parametrize("str2, expected", [
         ('bb', [False, False, True]),
@@ -52,7 +54,7 @@ class TestStr:
     ])
     def test_endswith(self, str2, expected):
         result = FAString(['abab', 'ababa', 'abababb']).endswith(str2)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
     @parametrize("str2, expected", [
         ('A', [True, True, False, False, False]),
@@ -63,10 +65,10 @@ class TestStr:
     ])
     def test_contains(self, str2, expected):
         result = FAString(SYMBOLS).contains(str2)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
         result = FAString(NB_PARALLEL_SYMBOLS).contains(str2)
-        assert np.array_equal(result, expected * 2000)
+        assert_array_equal(result, expected * 2000)
 
     @parametrize("str2, expected", [
         ('A', [0, 0, -1, -1, -1]),
@@ -78,30 +80,30 @@ class TestStr:
     ])
     def test_strstr(self, str2, expected):
         result = FAString(SYMBOLS).strstr(str2)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
         result = FAString(NB_PARALLEL_SYMBOLS).strstr(str2)
-        assert np.array_equal(result, expected * 2000)
+        assert_array_equal(result, expected * 2000)
 
     def test_strstr_cat(self):
         result = self.cat_symbol.str.strstr('A')
         expected = FA([np.iinfo(np.int32).min, 0, 0, -1, -1, -1] * 3)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
     def test_strlen_cat(self):
         result = self.cat_symbol.str.strlen
         expected = FA([np.iinfo(np.int32).min, 4, 4, 2, 4, 3] * 3)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
     def test_strlen_parallel(self):
         result = FAString(NB_PARALLEL_SYMBOLS).strlen
         expected = FA([4, 4, 2, 4, 3] * 2000)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
     def test_strpbrk_cat(self):
         result = self.cat_symbol.str.strpbrk('PZG')
         expected = FA([np.iinfo(np.int32).min, 2, 2, -1, 0, -1] * 3)
-        assert np.array_equal(result, expected)
+        assert_array_equal(result, expected)
 
     regex_match_test_cases = parametrize('regex, expected', [
         ('.', [True] * 5),
@@ -114,12 +116,12 @@ class TestStr:
     @regex_match_test_cases
     def test_regex_match(self, regex, expected):
         fa = FA(SYMBOLS)
-        assert np.array_equal(fa.str.regex_match(regex), expected)
+        assert_array_equal(fa.str.regex_match(regex), expected)
 
     @regex_match_test_cases
     def test_regex_match_cat(self, regex, expected):
         cat = Cat(SYMBOLS * 2)   # introduce duplicity to test ikey properly
-        assert np.array_equal(cat.str.regex_match(regex), expected * 2)
+        assert_array_equal(cat.str.regex_match(regex), expected * 2)
 
     substr_test_cases = parametrize("start_stop", [
         (0, 1),
@@ -143,14 +145,14 @@ class TestStr:
     def test_substr_bytes(self, start_stop):
         expected = [s[slice(*start_stop)] for s in SYMBOLS]
         result = FAString(FastArray(SYMBOLS)).substr(*start_stop)
-        assert (FastArray(expected) == result).all()
+        assert_array_equal(FastArray(expected), result)
 
     @substr_test_cases
     def test_substr_cat(self, start_stop):
         result = self.cat_symbol.str.substr(*start_stop)
         expected = [s[slice(*start_stop)] for s in SYMBOLS]
         expected = Categorical(self.cat_symbol.ikey, expected, base_index=1)
-        assert (expected == result).all()
+        assert_array_or_cat_equal(expected, result, relaxed_cat_check=True)
         # check categories are unique
         assert len(set(result.category_array)) == len(result.category_array)
 
@@ -158,14 +160,14 @@ class TestStr:
     def test_char(self, position):
         result = FAString(SYMBOLS).char(position)
         expected = [s[position] if position < len(s) else '' for s in SYMBOLS]
-        assert expected == result.tolist()
+        assert result.tolist() == expected
 
     @parametrize('position', [0, 1, -1, -2])
     def test_char_cat(self, position):
         result = self.cat_symbol.str.char(position)
         expected = Categorical(self.cat_symbol.ikey, [s[position] for s in SYMBOLS],
                                base_index=1)
-        assert (expected == result).all()
+        assert_array_or_cat_equal(expected, result, relaxed_cat_check=True)
         # check categories are unique
         assert len(set(result.category_array)) == len(result.category_array)
 
@@ -173,7 +175,7 @@ class TestStr:
         position = [-1, 2, 0, 1, 2]
         result = FAString(SYMBOLS).char(position)
         expected = [s[pos] for s, pos in zip(SYMBOLS, position)]
-        assert expected == result.tolist()
+        assert result.tolist() == expected
 
     @parametrize('position', [
         -3, 6, [0, 0, 0, 0, -5]
