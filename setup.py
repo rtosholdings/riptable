bdb0b8f59ec5d5c67779ea2afd838d5758798f26
OrestZborowski-SIG <orest.zborowski@sig.com>
2022-02-23 10:57:26 -0500

    Update riptable/riptide_cpp build/run constraints (#297)
    Update the current (master) riptable/riptide_cpp build/run constraints to the following:
    
    OS: Linux, Windows (drop MacOS)
    Python 3.8, 3.9, 3.10
    numpy >=1.20 <1.22
    numba >=0.51
    
    Riptable and riptide_cpp would have the same constraints (so trim riptide_cpp to fit)
    Need to update the conda build and pypy builds with the same constraints.
    
    For reference the NumPy support policy is outlined in NEP-29

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
     classifiers=[
          "Development Status :: 4 - Beta",
          "Programming Language :: Python :: 3",
-         "Programming Language :: Python :: 3.6",
-         "Programming Language :: Python :: 3.7",
          "Programming Language :: Python :: 3.8",
+         "Programming Language :: Python :: 3.9",
+         "Programming Language :: Python :: 3.10",
          "License :: OSI Approved :: BSD License",
          "Operating System :: OS Independent",
     ]
